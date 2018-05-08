e187053746f56c4838e465deb4657d1f737e8407
keery <mon-compte1131@live.fr>
2018-05-08 19:30:08 +0200

    Convert an unit in gram, in order to calculate the factor

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,10 +8,10 @@ const UNITS = [
   { id : '6pce' , name: 'LOT DE 6 PCE', quantity: 6, referenceUnit: 'pce'},
   { id : '4pce' , name: 'LOT DE 4 PCE', quantity: 4, referenceUnit: 'pce'},
   { id : '3pce' , name: 'LOT DE 3 PCE', quantity: 3, referenceUnit: 'pce'},
-  { id : 'pce' , name: 'PCE', quantity: 3, referenceUnit: 'pce'},
+  { id : 'pce' , name: 'PCE', quantity: 3, referenceUnit: 'kg'},
   { id : 'kg' , name: 'KG', quantity: 1000, referenceUnit: 'g'},
-  { id : 'g' , name: 'g', quantity: null, referenceUnit: null},
-  { id : 'cs' , name: 'CS', quantity: 3, referenceUnit: 'g'},
+  { id : 'g' , name: 'g', quantity: 1, referenceUnit: null},
+  { id : 'cs' , name: 'CS', quantity: 20, referenceUnit: 'g'},
 ]
 
 class Unit {
@@ -19,7 +19,7 @@ class Unit {
     this.id = id;
     this.name = name;
     this.quantity = quantity;
-    this.ref = ref;
+    this.referenceUnit = ref;
   }
 }
 
@@ -28,14 +28,18 @@ class App extends Component {
   state = {
     units : this.createUnits(),
     first : null,
-    second : null
+    second : null,
+    factor : 0
   }
+
   constructor(props) {
     super(props)
     this.handleUnitFirst = this.handleUnitFirst.bind(this)
     this.handleUnitSecond = this.handleUnitSecond.bind(this)
+    this.convertUnit = this.convertUnit.bind(this)
   }
 
+  //Crée et retourne un tableau d'objet Unit
   createUnits() {
     const t_unit = [];
     for (let i in UNITS) {
@@ -51,21 +55,57 @@ class App extends Component {
     this.setState({first: this.state.units[0]})
     this.setState({second: this.state.units[1]})
   }
-  convertUnit(firstUnit, secondUnit) {
 
+  //Calcule le facteur des deux units
+  convertUnit() {
+    const { first, second } = this.state;
+    
+    const firstInGram = this.convertToGram(first);
+    const secondInGram = this.convertToGram(second);
+
+    this.updateFactor((firstInGram / secondInGram))
   }
 
+  //Convert une unité en gramme
+  convertToGram(unit) {
+
+    if(unit.id === 'g' || unit.referenceUnit === 'g') {
+      return unit.quantity
+    }
+    else  return this.recursUnit(unit)
+  }
+
+  //Recurs sur les units pour trouver la quantité de depart en gramme
+  recursUnit(unit, quantity=null){
+    const { units } = this.state;
+    
+    if(unit.id === 'g') return quantity;
+    else {
+      const refId = units.findIndex((el) => el.id === unit.referenceUnit)
+      quantity = (quantity ? quantity : unit.quantity);
+      return this.recursUnit(units[refId], (quantity * units[refId].quantity))
+    }
+    
+  }
+
+  //Met à jour le facteur à afficher
+  updateFactor(newFactor) {
+    this.setState({factor: newFactor})
+  }
+
+  //Assigne le premier unit  
   handleUnitFirst(event) {
     this.setState({first: this.state.units.filter(x => x.id === event.target.value)[0] });
   }
 
+  //Assigne le deuxième unit
   handleUnitSecond(event) {
     this.setState({second: this.state.units.filter(x => x.id === event.target.value)[0] });
   }
 
 
   render() {
-    const { units, first, second } = this.state
+    const { units, first, second, factor } = this.state
 
     return (
         <section className="App">
@@ -88,10 +128,14 @@ class App extends Component {
               }
             </select>
             <div className="spacing-h">
-              <button>Convert</button>
+              <button onClick={this.convertUnit}>Convert</button>
             </div>
           </div>
-          
+          {
+            factor !== 0 && (
+              <b className="factor">{factor}</b>
+            )
+          }
         </section>
     );
   }
